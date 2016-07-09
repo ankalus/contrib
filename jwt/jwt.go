15c918bc753cd4bdfb9309ab31896ad1901ce395
Grzegorz Wiciak <grzegorz@wiciak.com>
2016-07-09 22:12:18 +0200

    Update jtw

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,13 +1,14 @@
 package jwt
 
 import (
-	jwt_lib "github.com/dgrijalva/jwt-go/request"
+	jwt_lib "github.com/dgrijalva/jwt-go"
+	jwt_request_lib "github.com/dgrijalva/jwt-go/request"
 	"github.com/gin-gonic/gin"
 )
 
 func Auth(secret string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		_, err := jwt_lib.ParseFromRequest(c.Request, func(token *jwt_lib.Token) (interface{}, error) {
+		_, err := jwt_request_lib.ParseFromRequest(c.Request, func(token *jwt_lib.Token) (interface{}, error) {
 			b := ([]byte(secret))
 			return b, nil
 		})
